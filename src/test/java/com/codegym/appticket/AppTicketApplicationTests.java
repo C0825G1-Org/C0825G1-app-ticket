6f3dcd9886584fb8a0357fc5ee25d82783d9df65
Tri-Phung <triphung15@gmail.com>
2026-01-12 09:19:11 +0700

    Initial project setup: entities, templates structure

---
+++ src/test/java/com/codegym/appticket/AppTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.appticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
